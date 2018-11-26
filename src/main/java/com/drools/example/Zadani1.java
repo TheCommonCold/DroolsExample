428c3ebf5c86c499af73fa7730811242a279081b
Luker <luker.plus@gmail.com>
2018-11-26 20:30:53 +0100

    Initial commit

---
+++ src/main/java/com/drools/example/Zadani1.java
@@ -0,0 +1,26 @@
+package com.drools.example;
+import java.io.IOException;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.drools.compiler.compiler.DroolsParserException;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+public class Zadani1 {
+    public static void main(String[] args) throws DroolsParserException,
+            IOException {
+        Zadani1 zadani1 = new Zadani1();
+        zadani1.executeHelloWorldRules();
+    }
+    public void executeHelloWorldRules() throws IOException,
+            DroolsParserException {
+        KieServices ks = KieServices.Factory.get();
+        BasicConfigurator.configure();
+        Logger.getLogger(Zadani1.class).setLevel(Level.OFF);
+        KieContainer kContainer = ks.getKieClasspathContainer();
+        KieSession session = kContainer.newKieSession("ksession-rules");
+        session.fireAllRules();
+    }
+}
+
