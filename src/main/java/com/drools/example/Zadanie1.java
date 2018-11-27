57137d19e1aa5e43da536c7097640af329c900f9
Vulwsztyn <artur.mostowski@student.put.poznan.pl>
2018-11-27 19:05:44 +0100

    Works as Nintendo

--- src/main/java/com/drools/example/Zadanie1.java
+++ src/main/java/com/drools/example/Zadanie1.java
@@ -7,17 +7,17 @@ import org.drools.compiler.compiler.DroolsParserException;
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
-public class Zadani1 {
+public class Zadanie1 {
     public static void main(String[] args) throws DroolsParserException,
             IOException {
-        Zadani1 zadani1 = new Zadani1();
+        Zadanie1 zadani1 = new Zadanie1();
         zadani1.executeHelloWorldRules();
     }
     public void executeHelloWorldRules() throws IOException,
             DroolsParserException {
         KieServices ks = KieServices.Factory.get();
         BasicConfigurator.configure();
-        Logger.getLogger(Zadani1.class).setLevel(Level.OFF);
+        Logger.getLogger(Zadanie1.class).setLevel(Level.OFF);
         KieContainer kContainer = ks.getKieClasspathContainer();
         KieSession session = kContainer.newKieSession("ksession-rules");
         session.fireAllRules();
